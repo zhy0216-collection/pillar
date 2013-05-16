c681e5e66aa4e5689824a15b3eda63abffb09136
Yang <zhy0216@gmail.com>
2013-05-16 21:30:55 +0800

    format config

--- projectname/config/conf.py
+++ projectname/config/conf.py
@@ -3,24 +3,24 @@
 import logging
 import datetime as dt
 
-DEBUG_MODE = False
-LESS_DEBUG = False
-JS_DEBUG = False
-PRODUCTION = False
+DEBUG_MODE          = True
+LESS_DEBUG          = True
+JS_DEBUG            = True
+PRODUCTION          = False
 
-SITENAME = "projectname"#your site here
-SITE_DOMAIN = ""#your site here
-PORT = "5000"
-SECRET_KEY = "secret_keyplzchangeit"
+SITENAME            = "projectname"#your site here
+SITE_DOMAIN         = ""#your site here
+PORT                = "5000"
+SECRET_KEY          = "secret_keyplzchangeit"
 
-MONGODB_DB = "zion"
-MONGODB_DB_UNITTEST = "zion_unittest"
-MONGODB_HOST = "localhost"
-MONGODB_PORT = 27017
-MONGODB_USER = 'test'
-MONGODB_PASSWD = 'test'
+MONGODB_DB          = ""#project db
+MONGODB_DB_UNITTEST = "zion_unittest"#project unittest
+MONGODB_HOST        = "localhost"
+MONGODB_PORT        = 27017
+MONGODB_USER        = ''#your mongodb username; keep it empty if no auth required
+MONGODB_PASSWD      = ''#your mongodb pwd; keep it empty if no auth required
 
-SENTRY_DSN = ''#somethinf about sentry
+SENTRY_DSN          = ''#somethinf about sentry
 
 
 try:
