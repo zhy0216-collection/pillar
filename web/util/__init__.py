c2665a0f4e4a5476f178eb2ec2649d317dcd6f33
Yang <zhyang@xiaomi.com>
2013-11-19 19:47:07 +0800

    add login_required

--- web/util/__init__.py
+++ web/util/__init__.py
@@ -1,1 +1,19 @@
 ﻿# -*- coding: utf-8 -*-
+
+from functools import wraps
+
+from flask import session, request, redirect, url_for
+
+
+
+#http://flask.pocoo.org/docs/patterns/viewdecorators/
+def login_required(f):
+    from model import User
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        r = request.path
+        if "user" not in session:
+            return redirect(url_for('login', r=r))
+        return f(*args, **kwargs)
+    return decorated_function
+
