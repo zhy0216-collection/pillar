6f4af31452e56d6497330e7e2eeb6a307f17788d
Yang <zhyang@xiaomi.com>
2014-02-11 21:30:07 +0800

    use better required_login

--- web/util/__init__.py
+++ web/util/__init__.py
@@ -5,15 +5,26 @@ from functools import wraps
 from flask import session, request, redirect, url_for
 
 
-
 #http://flask.pocoo.org/docs/patterns/viewdecorators/
 def login_required(f):
-    from model import User
+    from web.model import User
+    from web.app import app
     @wraps(f)
     def decorated_function(*args, **kwargs):
         r = request.path
         if "user" not in session:
+            flash("You need to login first!", "danger")
             return redirect(url_for('login', r=r))
+        else:
+            g.user = User.get_user_by_id(session["user"]["id"])
+            if g.user is None:
+                return redirect(url_for('login', r=r))
+        def inject_user():
+            if "user" not in g:
+                g.user = User.get_user_by_id(session.get("user", {}).get("id"))
+            return dict(cur_user=g.user)
+        app.template_context_processors[None].append(inject_user)
         return f(*args, **kwargs)
-    return decorated_function
 
+    return decorated_function
+    
