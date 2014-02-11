562f59a3f0eb1ef3b38bee6f6d8423e81722cdfe
Yang <zhyang@xiaomi.com>
2014-02-11 22:04:46 +0800

    add sentry

--- web/app.py
+++ web/app.py
@@ -2,22 +2,26 @@ import os
 
 import flask as f
 from flask import Flask
-import flask.ext.assets as fassets
-
+from flask.ext.assets import Environment, Bundle
+from raven.contrib.flask import Sentry
 import config.conf as conf
 
 app = Flask(__name__)
 
 app.config.from_object("web.config.conf")
+sentry = Sentry(dsn=conf.SENTRY_DSN)
 
-assets = fassets.Environment(app)
+if not app.config["DEBUG_MODE"]:
+    sentry.init_app(app)
+
+assets = Environment(app)
 assets.versions = 'hash:32'
-main_js = fassets.Bundle("main_bundle.js", 
-                        output='dist/main_bundle.%(version)s.js')
+main_js = Bundle("main_bundle.js", 
+                 output='dist/main_bundle.%(version)s.js')
 assets.register('js_all', main_js)
 
-all_css = fassets.Bundle("style.css", 
-                         output='dist/main.%(version)s.css')
+all_css = Bundle("style.css", 
+                 output='dist/main.%(version)s.css')
 assets.register('css_all', all_css)
 
 
